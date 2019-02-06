df892878fccf85b84eb9108299f494b08034ddd9
Guilherme Rogatto <gui.rogatto@gmail.com>
2019-02-06 14:09:04 -0200

    First commit

---
+++ automacao_docker.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "Executando processo de automacao!"
+cd /tmp/workspace/build
+nohup json-server --watch db.json > my.log 2>&1 &
+echo $! > save_pid.txt
+newman run /tmp/workspace/build/Regression_Cateno_1.postman_collection.json -e /tmp/workspace/build/Cateno.postman_environment.json --export-environment /tmp/workspace/temporario/Cateno.postman_environment2.json --reporters html,cli --reporter-html-template /tmp/workspace/build/htmlreqres.hbs --reporter-html-export /tmp/workspace/build/Relatorios/primeira_rotina_api_cateno-file.html --ignore-redirects
+cd /tmp/workspace/gmail
+mvn clean test
+newman run /tmp/workspace/build/Regression_Cateno_2.postman_collection.json -e /tmp/workspace/temporario/Cateno.postman_environment2.json --export-environment /tmp/workspace/temporario/Cateno.postman_environment3.json --reporters html,cli --reporter-html-template /tmp/workspace/build/htmlreqres.hbs --reporter-html-export /tmp/workspace/build/Relatorios/segunda_rotina_api_cateno-file.html --ignore-redirects
+cd /tmp/workspace/cateno_automation
+mvn clean test
+cd /tmp/workspace/build
+kill -9 `cat save_pid.txt`
+rm save_pid.txt
+exit
